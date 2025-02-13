9fce719c232332002044fd7f5f7e9cd4b6f5e138
William Wingate <wtwingate@gmail.com>
2025-02-12 22:40:43 -0500

    Create separate webpack configs for dev and prod

--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -29,7 +24,7 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        test: /\.webp$/i,
         type: "asset/resource",
       },
     ],
